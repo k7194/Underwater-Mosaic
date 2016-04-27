1071b38c31862fd3ff21a45814bb2a9fb0bffc4b
kim <21125071@student.uwa.edu.au>
2016-04-27 21:51:31 +0800

    added Stitching Example code

---
+++ CMakeFiles/StitchImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/StitchImage.dir/StitchImage.cpp.o"
+  "StitchImage.pdb"
+  "StitchImage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/StitchImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ StitchImage.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>    
+#include <opencv2/opencv.hpp>    
+#include <opencv2/stitching/stitcher.hpp>  
+  
+#ifdef _DEBUG    
+#pragma comment(lib, "opencv_core246d.lib")     
+#pragma comment(lib, "opencv_imgproc246d.lib")   //MAT processing    
+#pragma comment(lib, "opencv_highgui246d.lib")    
+#pragma comment(lib, "opencv_stitching246d.lib");  
+  
+#else    
+#pragma comment(lib, "opencv_core246.lib")    
+#pragma comment(lib, "opencv_imgproc246.lib")    
+#pragma comment(lib, "opencv_highgui246.lib")    
+#pragma comment(lib, "opencv_stitching246.lib");  
+#endif    
+  
+using namespace cv;    
+using namespace std;  
+  
+  
+int main()    
+{  
+ vector< Mat > vImg;  
+ Mat rImg;  
+  
+ vImg.push_back( imread("./stitching_img/S1.jpg") );  
+ vImg.push_back( imread("./stitching_img/S2.jpg") );  
+// vImg.push_back( imread("./stitching_img/S3.jpg") );  
+// vImg.push_back( imread("./stitching_img/S4.jpg") );  
+// vImg.push_back( imread("./stitching_img/S5.jpg") );  
+  
+ Stitcher stitcher = Stitcher::createDefault();  
+  
+  
+ unsigned long AAtime=0, BBtime=0; //check processing time  
+ AAtime = getTickCount(); //check processing time  
+  
+ Stitcher::Status status = stitcher.stitch(vImg, rImg);  
+  
+ BBtime = getTickCount(); //check processing time   
+ printf("%.2lf sec \n",  (BBtime - AAtime)/getTickFrequency() ); //check processing time  
+  
+ if (Stitcher::OK == status){   
+  imshow("Stitching Result",rImg);  
+  imwrite("./stitching_img/Mosaic.jpg", rImg);
+ }
+  
+ else  
+  printf("Stitching fail.");  
+  
+ waitKey(0);  
+
+ return 1;  
+} 
